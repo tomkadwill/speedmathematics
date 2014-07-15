b7388078003d03af566029cdfb89c7380daa6926
Tom Kadwill <tomkadwill@gmail.com>
2014-07-15 09:18:51 +0100

    Added chapter 1 & 2 to seed file

--- db/fixtures/development/questions.rb
+++ db/fixtures/development/questions.rb
@@ -1,5 +1,12 @@
 #chapter 1
 Question.seed(:id, 
-  { id: 1, chapter: 1, question_one_lower: 90, question_one_upper: 99, question_two_lower: 90, question_two_upper: 99 },
+  { id: 1, chapter: 1, question_one_lower: 95, question_one_upper: 99, question_two_lower: 95, question_two_upper: 99 },
   { id: 2, chapter: 1, question_one_lower: 5, question_one_upper: 9, question_two_lower: 5, question_two_upper: 9 },
+)
+#chapter 2
+Question.seed(:id, 
+  { id: 3, chapter: 2, question_one_lower: 95, question_one_upper: 99, question_two_lower: 95, question_two_upper: 99 },
+  { id: 4, chapter: 2, question_one_lower: 5, question_one_upper: 9, question_two_lower: 5, question_two_upper: 9 },
+  { id: 5, chapter: 2, question_one_lower: 12, question_one_upper:18, question_two_lower: 12, question_two_upper: 18 },
+  { id: 6, chapter: 2, question_one_lower: 101, question_one_upper: 125, question_two_lower: 101, question_two_upper: 125 },
 )
