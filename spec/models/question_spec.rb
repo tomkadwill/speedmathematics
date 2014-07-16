ab99cf8d4a76b19c6d10536c57276f33efa3a260
Tom Kadwill <tomkadwill@gmail.com>
2014-07-16 09:23:56 +0100

    added specs for first_random and second_random

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,20 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
 
+	before do
+		@question = FactoryGirl.create :question
+	end
+
 	describe '.get' do
-		before do
-			@first_question = FactoryGirl.create :question
-		end
-		
 		it 'returns question record of matching chapter' do
-			expect(Question.get(1).chapter).to eq(@first_question.chapter)
+			expect(Question.get(1).chapter).to eq(@question.chapter)
 		end
 	end
 
 	describe '#formatted' do
 		it 'returns formatted question string' do
-			@question = FactoryGirl.build :question
 			expect(@question.formatted).to eq('92 x 92')
+		end
+	end
+
+	context 'test random' do
+		before do
+			@question.question_one_upper = 95
+			@question.question_two_upper = 95
+			@question.save
+		end
+
+		describe '#first_random' do
+			it 'returns first random number' do
+				expect(@question.first_random).to be < 96
+				expect(@question.first_random).to be > 91
+			end
+		end
+
+		describe '#second_random' do
+			it 'returns second random number' do
+				expect(@question.first_random).to be < 96
+				expect(@question.first_random).to be > 91
+			end
 		end
 	end
 end
